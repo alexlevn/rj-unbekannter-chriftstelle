72dcc7c1b6cde4aad7434d9754146ea44d877db7
Alex Lee <dung.le@snapmart.ph>
2022-05-23 21:23:46 +0700

    Updates

---
+++ precache-manifest.ce1d2b656bf766f97f38258d7edecafb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb0675c5345928b7d44eb083ee20611",
+    "url": "/rj-unbekannter-chriftstelle/index.html"
+  },
+  {
+    "revision": "9d6127adfc613ee43284",
+    "url": "/rj-unbekannter-chriftstelle/static/css/2.8315e4cf.chunk.css"
+  },
+  {
+    "revision": "d59c72e4eecc236985d2",
+    "url": "/rj-unbekannter-chriftstelle/static/css/main.a539d727.chunk.css"
+  },
+  {
+    "revision": "9d6127adfc613ee43284",
+    "url": "/rj-unbekannter-chriftstelle/static/js/2.0a8533fa.chunk.js"
+  },
+  {
+    "revision": "2ca78df4e200ab631a9acbb7a85ee5b8",
+    "url": "/rj-unbekannter-chriftstelle/static/js/2.0a8533fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d59c72e4eecc236985d2",
+    "url": "/rj-unbekannter-chriftstelle/static/js/main.f25ce820.chunk.js"
+  },
+  {
+    "revision": "7e5584527329eb303b9d",
+    "url": "/rj-unbekannter-chriftstelle/static/js/runtime-main.0373fed0.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/rj-unbekannter-chriftstelle/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/rj-unbekannter-chriftstelle/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/rj-unbekannter-chriftstelle/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/rj-unbekannter-chriftstelle/static/media/slick.f97e3bbf.svg"
+  }
+]);
